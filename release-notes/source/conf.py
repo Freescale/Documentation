db7da7aff2554fab875b708a80e18beaf27ce793
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2014-09-19 17:28:21 -0300

    release notes layout: use a custom "agogo" template
    The new template has been customized to allow the "table of contents"
    div to remain on the right of the page, while flowing the page contents
    around it.
    
    Signed-off-by: Lucas Dutra Nunes <ldnunes@ossystems.com.br>

--- release-notes/source/conf.py
+++ release-notes/source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'agogo'
 html_style = 'release-notes.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
