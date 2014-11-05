92323ae67c6ca5a4955cfe55a2a1ddfe25b8d79f
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2014-11-05 11:52:18 -0200

    script: create doc_utils.py module for commonly used methods
    For now it only has "tabularize", a method extracted from
    scripts/bitbake-metadata2doc.py that creates reST tables from
    bi-dimensional lists.
    
    Also, the method is slightly different from the original, as it applies
    rstrip() to the generated table header and rows to avoid polluting the
    commit diffs with blank spaces.
    
    Signed-off-by: Lucas Dutra Nunes <ldnunes@ossystems.com.br>

--- scripts/bitbake-metadata2doc.py
+++ scripts/bitbake-metadata2doc.py
@@ -8,6 +8,7 @@ import pickle
 import subprocess
 import copy
 import shutil
+from doc_utils import tabularize
 
 def info(fmt, *args):
     print(fmt % args)
@@ -17,36 +18,6 @@ def warn(fmt, *args):
 
 def error(fmt, *args):
     sys.stderr.write(('ERROR: ' + fmt + '\n') % args)
-
-def tabularize(lines, spacing=2):
-    def format_border(widths):
-        return spc.join([ '=' * width for width in widths ])
-
-    def format_header(header, widths, spc):
-        border = format_border(widths)
-        header = spc.join(map(lambda col, width: col.ljust(width),
-                              header, widths))
-        return '\n'.join([border, header, border])
-
-    def sort_by_col(lines, col):
-        return sorted(lines, key=lambda l: l[col])
-
-    def format_body(lines, widths, spc):
-        def format_line (line):
-            return spc.join(map(lambda col, width: col.ljust(width),
-                                line, widths))
-        return "\n".join(map(format_line, sort_by_col(lines, 0)))
-
-    spc = ' ' * spacing
-    if lines:
-        col_widths = map(lambda col: apply(max, map(len, col)),
-                         apply(zip, lines))
-        return '\n'.join([format_header(lines[0], col_widths, spc),
-                          format_body(lines[1:], col_widths, spc),
-                          format_border(col_widths)]) + \
-               '\n'
-    else:
-        return ""
 
 def describe(items):
     text = ''
